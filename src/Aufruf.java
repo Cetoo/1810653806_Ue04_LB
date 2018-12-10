c56d507a326739108550e8d1e9a89cf3272954d4
Cetoo <1810653806@stud.fh-kufstein.ac.at>
2018-12-10 16:49:46 +0100

    Aufgabe 5 in Aufruf

--- src/Aufruf.java
+++ src/Aufruf.java
@@ -11,7 +11,7 @@ public class Aufruf
 
         Date released = new Date();
         Book b1 = new Book(200, released, "My Book", "ISBN");
-        System.out.println("Aufgabe Nummer 3");
+        System.out.println("Aufgabe 3");
         System.out.println("Das Buch hat " + b1.getPages() + " Seiten, wurde am " + b1.getReleased() + " veröffentlicht und hat den Titel " + b1.getTitle());
         System.out.println("_______________________________________________________________________________________________________");
         //System.out mit b1 für meine Instanz anzuwählen und mit dem . spreche ich die Methode an die () übernehmen den zuvor übergebenen Wert
@@ -44,6 +44,36 @@ public class Aufruf
         for (Book item : listA)
         {
             System.out.println(item.getTitle() + " hat " + item.getPages() + " Seiten und folgende ISBN: " + item.getIsbn());
+        }
+
+        System.out.println("______________________________________________________________________________________________");
+
+
+        System.out.println("Aufgabe 5");
+        //Aufgabe 5
+        Date releasedate = new Date();
+
+        //Meine neuen Instanzen für Paperbook
+        Paperbook c1 = new Paperbook(200, releasedate,"My Paperbook 1", "1", "April");
+        Paperbook c2 = new Paperbook(210,releasedate, "My Paperbook 2", "2", "April");
+        Paperbook c3 = new Paperbook(240,releasedate, "My Paperbook 3", "3","January");
+        Paperbook c4 = new Paperbook(100,releasedate, "My Paperbook 4", "4", "March");
+        Paperbook c5 = new Paperbook(140,releasedate, "My Paperbook 5", "5", "October");
+
+
+        //Meine neue Liste für Paperbook
+        List<Paperbook> listB = new ArrayList();
+        listB.add(c1);
+        listB.add(c2);
+        listB.add(c3);
+        listB.add(c4);
+        listB.add(c5);
+
+
+        //For each list für Paperbook und diese läuft solange wie ich Instanzen in Paperbook habe
+        for (Paperbook item : listB)
+        {
+            System.out.println(item.getTitle() + " hat " + item.getPages() + " Seiten und folgende ISBN " + item.getIsbn() + " zusätzlich ist es im Monat " + item.getMonth() + " erschienen.");
         }
     }
 }
--- src/Paperbook.java
+++ src/Paperbook.java
@@ -38,39 +38,13 @@ public class Paperbook extends Book
 
 
     //Konstruktur für Paperbook + String series und String month hinzufügen
-    public Paperbook(int pages, Date released, String title, String isbn, String series, String month)
+    //Ich entferne bei meinem Konstruktor ISBN als Übergabeparameter weil ich bei super einen fixen Wert übergebe
+    public Paperbook(int pages, Date released, String title, String series, String month)
     {
         //Mit super spreche ich die Klasse an von der diese Klasse geerbt hat in diesem Fall spreche ich die Klasse Book an
-        super(pages, released, title, isbn);
+        //Ich gebe ihm als ISBN gleich den Wert über da alle Instanzen die selbe Nummer haben
+        super(pages, released, title, "00000-000000-0000");
         this.series = series;
         this.month = month;
-    }
-
-    public static void main(String[] args)
-    {
-        Date releasedate = new Date();
-
-        //Meine neuen Instanzen für Paperbook
-        Paperbook c1 = new Paperbook(200,releasedate, "My Paperbook 1", "00000-000000-0000","1","April");
-        Paperbook c2 = new Paperbook(210,releasedate, "My Paperbook 2", "00000-000000-0000", "2", "April");
-        Paperbook c3 = new Paperbook(240,releasedate, "My Paperbook 3", "00000-000000-0000", "3", "January");
-        Paperbook c4 = new Paperbook(100,releasedate, "My Paperbook 4", "00000-000000-0000", "4", "March");
-        Paperbook c5 = new Paperbook(140,releasedate, "My Paperbook 5", "00000-000000-0000", "5", "October");
-
-
-        //Meine neue Liste für Paperbook
-        List<Paperbook> listB = new ArrayList();
-        listB.add(c1);
-        listB.add(c2);
-        listB.add(c3);
-        listB.add(c4);
-        listB.add(c5);
-
-
-        //For each list für Paperbook und diese läuft solange wie ich Instanzen in Paperbook habe
-        for (Paperbook item : listB)
-        {
-            System.out.println(item.getTitle() + " hat " + item.getPages() + " Seiten und folgende ISBN " + item.getIsbn() + " zusätzlich ist es im Monat " + item.getMonth() + " erschienen.");
-        }
     }
 }
